2af3355e75644f0be7be788224293bc5a1cb2754
heloisaborba <hbbs@cesar.school>
2025-11-09 14:19:26 -0300

    correcao de erro multiplas definicoes

---
+++ include/game.h
@@ -0,0 +1,12 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "raylib.h"
+
+
+void InitGame(void);
+void UpdateGame(void);
+void DrawGame(void);
+void CloseGame(void);
+
+#endif
--- src/game.c
+++ src/game.c
@@ -4,20 +4,3 @@
 #include "enemy.h"
 #include "hero.h"
 
-Texture2D background;
-
-void InitGame() {
-    // Carregar imagem do fundo
-    background = LoadTexture("resources/background.png");
-}
-
-void DrawGame() {
-    // Desenhar o fundo
-    DrawTexture(background, 0, 0, WHITE);
-
-    // Depois desenha heróis, torres, inimigos por cima
-}
-
-void CloseGame() {
-    UnloadTexture(background); // liberar memória
-}
